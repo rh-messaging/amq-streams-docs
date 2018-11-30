746102705cfe7a7d4a6f30cb8b89ae9df7235fc8
Tom Bentley <tombentley@users.noreply.github.com>
2018-11-30 13:35:18 +0000

    Use make to build, stage the docs (#32)

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 
-SOURCES=books
-RELEASE_VERSION=1.0
-
-./.travis/check_docs.sh "$SOURCES"
-CHECK=$?
-
-mkdir -p out/html
-cp -vrL "$SOURCES/images" out/html/images
-asciidoctor -v --failure-level WARN -t -dbook -a ProductVersion=${RELEASE_VERSION} "$SOURCES/master.adoc" -o out/html/index.html
-GEN=$?
-
-if [ "$CHECK" != "0" -o "$GEN" != "0" ]; then
-  echo "Documentation errors: check output above"
-  exit 1
-fi
-
+make buildall check
